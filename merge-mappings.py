a18d93dab2fbf9a486a1da5cae5995bf9eed4d65
Donald Hunter <donald.hunter@gmail.com>
2024-06-14 11:49:07 +0100

    Script to merge DoD mappings into NIST controls
    Signed-off-by: Donald Hunter <donald.hunter@gmail.com>

--- merge-mappings.py
+++ merge-mappings.py
@@ -2,6 +2,60 @@
 
 from argparse import ArgumentParser
 import yaml
+import sys
+
+def read_dod_mappings(dod_file):
+    with open(dod_file) as d:
+        dod = yaml.safe_load(d)
+
+    mapping_by_id = {}
+    for section in dod:
+        name = section['name']
+        for m in section['mappings']:
+            key = m['key']
+            categories = m['categories']
+            if key not in mapping_by_id:
+                mapping_by_id[key] = []
+            mapping_by_id[key].append((name, categories))
+    return mapping_by_id
+
+
+def dod_props_from_mappings(mappings):
+    props = []
+    for (name, categories) in mappings:
+        for category in categories:
+            props.append({'name': name,
+                          'value': category,
+                          'class': 'dod-zero-trust-overlay'})
+    return props
+
+
+def merge_into_control(control, dod_mappings):
+    id = control['id'].upper()
+    if id in dod_mappings:
+        mappings = dod_mappings[id]
+        control['props'].extend(dod_props_from_mappings(mappings))
+    if 'controls' in control:
+        for subcontrol in control['controls']:
+            merge_into_control(subcontrol, dod_mappings)
+
+
+def merge_dod_mappings(nist_file, dod_file):
+
+    dod_mappings = read_dod_mappings(dod_file)
+
+    print(f"Reading {nist_file} ...", file=sys.stderr)
+    with open(nist_file) as f:
+        nist = yaml.safe_load(f)
+
+    print(f"Merging {dod_file} ...", file=sys.stderr)
+
+    for group in nist['catalog']['groups']:
+        for control in group['controls']:
+            merge_into_control(control, dod_mappings)
+
+    print("Writing new YAML ...", file=sys.stderr)
+    print(yaml.dump(nist))
 
 
 if __name__ == '__main__':
@@ -13,26 +67,4 @@ if __name__ == '__main__':
                         help='The DoD mappings')
     args = parser.parse_args()
 
-    with open(args.dod) as d:
-        dod = yaml.safe_load(d)
-    mapping_by_id = {}
-    for section in dod:
-        name = section['name']
-        for m in section['mappings']:
-            key = m['key']
-            categories = m['categories']
-            if key not in mapping_by_id:
-                mapping_by_id[key] = []
-            mapping_by_id[key].append((name, categories))
-
-    with open(args.file) as f:
-        nist = yaml.safe_load(f)
-
-    for group in nist['catalog']['groups']:
-        for control in group['controls']:
-            id = control['id'].upper()
-            if id in mapping_by_id:
-                mapping = mapping_by_id[id]
-                control['props'].append({'class': 'dod-zero-trust-overlay'})
-
-    print(yaml.dump(nist))
+    merge_dod_mappings(args.file, args.dod)
