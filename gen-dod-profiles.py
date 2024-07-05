58d0d4c9c204ee2c9c00cce7b3020e2b599e9180
Donald Hunter <donald.hunter@gmail.com>
2024-07-05 13:53:13 +0100

    Add tooltips and glossary of NIST groups to visualization
    Signed-off-by: Donald Hunter <donald.hunter@gmail.com>

--- gen-dod-profiles.py
+++ gen-dod-profiles.py
@@ -23,12 +23,54 @@ html_preamble = '''<!doctype html>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous">
+    <style>dt { float: left; width: 3em; } dd { margin-bottom: 0; }</style>
   </head>
   <body>
 '''
 
-html_postamble = '''<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+html_postamble = '''
+  <div class="container" style="padding-top: 2em;">
+    <div class="row">
+      <div class="col">
+        <dl>
+          <dt>ac</dt><dd>Access Control</dd>
+          <dt>at</dt><dd>Awareness and Training</dd>
+          <dt>au</dt><dd>Audit and Accountability</dd>
+          <dt>ca</dt><dd>Assessment, Authorization and Monitoring</dd>
+          <dt>cm</dt><dd>Configuration Management</dd>
+          <dt>cp</dt><dd>Contingency Planning</dd>
+          <dt>ia</dt><dd>Identification and Authentication</dd>
+          <dt>ir</dt><dd>Incident Response</dd>
+          <dt>ma</dt><dd>Maintenance</dd>
+          <dt>mp</dt><dd>Media Protection</dd>
+        </dl>
+      </div>
+      <div class="col">
+        <dl>
+          <dt>pe</dt><dd>Physical and Environmental Protection</dd>
+          <dt>pl</dt><dd>Planning</dd>
+          <dt>pm</dt><dd>Program Management</dd>
+          <dt>ps</dt><dd>Personnel Security</dd>
+          <dt>pt</dt><dd>Personally Identifiable Information Processing and Transparency</dd>
+          <dt>ra</dt><dd>Risk Assessment</dd>
+          <dt>sa</dt><dd>System and Services Acquisition</dd>
+          <dt>sc</dt><dd>System and Communications Protection</dd>
+          <dt>si</dt><dd>System and Information Integrity</dd>
+          <dt>sr</dt><dd>Supply Chain Risk Management</dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+          crossorigin="anonymous"></script>
+  <script>
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  </script>
   </body>
 </html>
 '''
@@ -123,11 +165,11 @@ def generate_html():
 
     html.append('<div class="container-fluid">')
     html.append('<div class="row align-items-end">')
-    html.append('  <div class="col"></div>')
+    html.append('<div class="col"></div>')
     for p in pillars:
-        html.append('  <div class="col">')
-        html.append(f'    <h5>{p}</h5>')
-        html.append('  </div>')
+        html.append('<div class="col">')
+        html.append(f'<h5>{p}</h5>')
+        html.append('</div>')
     html.append('</div>')
 
     for i, k in reversed(list(enumerate(baselines_by_name.keys()))):
@@ -137,22 +179,21 @@ def generate_html():
 
         baseline_ids = { id : id for id in baselines_by_name[k] }
 
-        html.append('<div class="row align-items-start">')
-        html.append('  <div class="col">')
-        html.append(f'    <h5>{head} Baseline</h5>')
-        html.append('  </div>')
+        html.append('<div class="row align-items-end">')
+        html.append('<div class="col">')
+        html.append(f'<h5>{head} Baseline</h5>')
+        html.append('</div>')
 
         for p in pillars:
-            html.append('  <div class="col">')
+            html.append('<div class="col">')
 
             for id in mappings[p]:
                 if id in baseline_ids:
-                    html.append(f'    <span class="{classes}">{id}</span>')
+                    text = controls_by_id[id]['title']
+                    html.append(f'<span class="{classes}" data-bs-toggle="tooltip" data-bs-title="{text}">{id}</span>')
 
-            html.append('  </div>')
-
+            html.append('</div>')
         html.append('</div>')
-
     html.append('</div>')
 
     return "\n".join(html)
